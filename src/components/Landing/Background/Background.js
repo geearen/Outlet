82f02d0981231bdec45b824524eb7952d4428d70
geearen <allengonzales018@gmail.com>
2021-11-17 23:56:20 -0800

    adds particles-react

---
+++ src/components/Landing/Background/Background.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react'
+import Particles from 'react-tsparticles';
+import particlesConfig from '../../../config/ParticleConfig';
+
+class Background extends Component {
+  constructor(props) {
+    super(props);
+
+    this.particlesInit = this.particlesInit.bind(this);
+    this.particlesLoaded = this.particlesLoaded.bind(this);
+  }
+
+  particlesInit(main) {
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  }
+
+  particlesLoaded(container) {}
+
+  render() {
+    return (
+      <Particles
+        id="tsparticles"
+        init={this.particlesInit}
+        loaded={this.particlesLoaded}
+        options={particlesConfig}
+      />
+    );
+  }
+}
+
+export default Background;
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Nav from '../components/Landing/NavBar/Navbar';
 import Footer from '../components/Landing/Footer/Footer';
+import Background from '../components/Landing/Background/Background';
 
 function LandingPage(){
   return(
     <div>
+      <Background/>
       <Nav/> 
       
       
