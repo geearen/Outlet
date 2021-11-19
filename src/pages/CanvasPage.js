5c539fe67c05a5b8ea2621cd288a358a96e3c8f2
lonnalu <lonnalu@gmail.com>
2021-11-18 17:20:07 -0800

    Color state changes on click

--- src/pages/CanvasPage.js
+++ src/pages/CanvasPage.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { useState } from 'react';
 import Canvas from '../components/Canvas/Canvas';
+import { Colors } from '../components/data/Colors';
 
 function CanvasPage(){
+  const [color, setColor] = useState('red');
+  
+  const handleColor = (selectedColor) => {
+    setColor(selectedColor);
+  }
+
   return(
     <>
       <h1>
         this is canvas
-        <a href="/"> home</a>
+        <a href='/'> home</a>
       </h1>
+      <div className='colors-container'>
+        {Colors.map((color) => {
+          return(
+            <div className='color-option' id={color} key={color} onClick={() => handleColor(color)}></div>
+          )
+        })}
+      </div>
       <Canvas />
     </>
   )
