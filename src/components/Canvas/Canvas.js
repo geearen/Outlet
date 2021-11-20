00572429f92d3d0bab13ad5b8be5b5856886f938
geearen <allengonzales018@gmail.com>
2021-11-20 15:33:20 -0800

    adds dynamicWidth and handleWidth method

--- src/components/Canvas/Canvas.js
+++ src/components/Canvas/Canvas.js
@@ -9,9 +9,12 @@ function Canvas() {
 
   const [ isDrawing, setIsDrawing ] = useState(false);
   const [currentColor, setCurrentColor] = useState("#000000");
+  const [currentWidth, setCurrentWidth] = useState(5);
 
   const selectedColor = useRef("#000000");
-  
+  const selectedLineWidth = useRef(5);
+  const direction = useRef(true);
+
   // @desc initializes canvas api when component is mounted
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -35,15 +38,30 @@ function Canvas() {
     contextRef.current.beginPath();
     contextRef.current.moveTo(offsetX, offsetY);
     contextRef.current.strokeStyle = selectedColor.current;
+    contextRef.current.lineWidth = selectedLineWidth.current;
     setCurrentColor(selectedColor.current);
+    dynamicLineWidth(selectedLineWidth.current);
+
 
     setIsDrawing(true);
   }
 
+  const dynamicLineWidth = useCallback(() =>{
+    if(!contextRef || !contextRef.current) return;
+
+    if(contextRef.current.lineWidth > 90 || contextRef.current.lineWidth <10){
+      direction.current = !direction.current;
+  }    
+    direction.current ? contextRef.current.lineWidth++ : contextRef.current.lineWidth--;
+    setCurrentWidth(contextRef.current.lineWidth);
+  },[]);
+
+
+
   const finishDrawing = () => {
     contextRef.current.closePath();
     setIsDrawing(false);
-  }
+  };
 
   const draw = ({ nativeEvent }) => {
     if (!isDrawing) {
@@ -53,16 +71,21 @@ function Canvas() {
     const { offsetX, offsetY } = nativeEvent;
     contextRef.current.lineTo(offsetX, offsetY);
     contextRef.current.stroke()
-  }
+  };
 
   const handleColor =(e) =>{
     setCurrentColor(e.currentTarget.value);
     selectedColor.current = e.currentTarget.value;
-  }
+  };
+
+  const handleWidth = (e) => {
+    setCurrentWidth(e.currentTarget.value);
+    selectedLineWidth.current = e.currentTarget.value;
+  };
 
   return(
     <>
-      <Toolbar handleColor={handleColor}/>
+      <Toolbar handleColor={handleColor} handleWidth={handleWidth}/>
       <canvas 
         onMouseDown={startDrawing}
         onMouseUp={finishDrawing}
--- src/components/Canvas/Toolbar.js
+++ src/components/Canvas/Toolbar.js
@@ -1,9 +1,21 @@
-function Toolbar({ handleColor }) {
+function Toolbar({ handleColor, handleWidth }) {
   return (
-    <div className='toolbar__color'>
-      <input type='color' onChange={handleColor} className='color-option' />
+    <div className="toolbar">
+      <div className="toolbar__color">
+        <input type="color" onChange={handleColor} className="color-option" />
+      </div>
+
+      <div className="toolbar__size">
+        <input
+          type="range"
+          defaultValue="5"
+          min="5"
+          max="100"
+          onChange={handleWidth}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
 export default Toolbar;
