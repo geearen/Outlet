42e67f9dbb0478d89084d8e029bdb13097ceb4e7
lonnalu <lonnalu@gmail.com>
2021-11-21 13:22:01 -0800

    Adds About Outlet section with responsiveness

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,6 +4,7 @@ import Footer from '../components/Landing/Footer/Footer';
 
 import LogoBackground from '../components/Landing/LogoBackground/LogoBackground';
 import LogoSection from '../components/Landing/LogoSection/LogoSection';
+import AboutOutlet from '../components/Landing/AboutOutlet/AboutOutlet';
 import Developer from '../components/Landing/Developers/Developer';
 
 function LandingPage(){
@@ -11,11 +12,9 @@ function LandingPage(){
     <>
         <div className="landing_page">
         <LogoBackground/>
-
-          
-
         <Navbar/> 
         <LogoSection/>  
+        <AboutOutlet />
         <Developer/>
         <Footer/>
           
