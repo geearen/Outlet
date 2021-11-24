77f199a731c46141148bb2a880aa2a3e7d91565c
lonnalu <lonnalu@gmail.com>
2021-11-23 21:55:27 -0800

    Updates the developer profile

--- src/components/data/DeveloperData.js
+++ src/components/data/DeveloperData.js
@@ -4,9 +4,9 @@ export const DeveloperData = [
     github: "https://github.com/Luna5678",
     email: "mailto:lonna.dev@gmail.com",
     portfolio: "https://lonnalu.com",
-    portrait: "https://thesource.com/wp-content/uploads/2021/11/FDIXJ42WQAAy9eV.jpeg",
+    portrait: "images/portrait-lonna.jpg",
     linkedin: "https://www.linkedin.com/in/lonna-lu/",
-    about: " I am a web developer. Praesent in mauris eu tortor porttitor accumsan. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Aenean fermentum risus id tortor. Integer imperdiet lectus quis justo. Aenean fermentum risus id tortor. Integer imperdiet lectus quis justo.",
+    about: "A fullstack developer who is passionate about giving back to the world by building online resources. With a background in project management, she is adept at working collaboratively and builds websites with the user in mind. She is meticulous and works thoughtfully to make ideas come to life.",
   },
   {
     name: "Allen Gonzales",
@@ -16,6 +16,6 @@ export const DeveloperData = [
     portrait:
       "https://res.cloudinary.com/dsxis0hux/image/upload/v1637386168/pics/20201227-MRJ01356_barxvr.jpg",
     linkedin: "https://www.linkedin.com/in/allen-gee/",
-    about: " I am a web developer. Praesent in mauris eu tortor porttitor accumsan. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Aenean fermentum risus id tortor. Integer imperdiet lectus quis justo. Aenean fermentum risus id tortor. Integer imperdiet lectus quis justo.",
+    about: "A software engineer with a background in avionics and a Bachelor's in Computer Engineering Technology. Allen has experience with troubleshooting, critical thinking, and problem solving. In addition, his love for Film and Digital Photography enables him to build captivating websites.",
   },
 ];
