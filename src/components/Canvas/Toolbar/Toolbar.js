5436d2823f83ee64b477693942500d2c628cf6de
lonnalu <lonnalu@gmail.com>
2021-11-24 10:29:40 -0800

    Adds mobile hamburger style to canvas toolbar

--- src/components/Canvas/Toolbar/Toolbar.js
+++ src/components/Canvas/Toolbar/Toolbar.js
@@ -2,7 +2,8 @@ import React, { useState, useRef } from 'react';
 import * as FaIcons from "react-icons/fa";
 import * as MdIcons from "react-icons/md";
 import * as AiIcons from 'react-icons/ai';
-import * as BiIcons from 'react-icons/bi'
+import * as BiIcons from 'react-icons/bi';
+import * as IoIcons from 'react-icons/io';
 import OutletLogo from '../../../assets/outlet_logo7.svg';
 
 function Toolbar({
@@ -24,7 +25,10 @@ function Toolbar({
   isEraser,
 }) 
 {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
   
+  const handleMobileMenu = () => setShowMobileMenu(!showMobileMenu);
+
   return (
     <div className="toolbar">
       <div className="canvas__logo">
@@ -34,7 +38,12 @@ function Toolbar({
           </a>
       </div>
 
-      <div className="toolbar__color">
+      <div className="mobile-toggle-icon" onClick={handleMobileMenu}>
+        {showMobileMenu ? <IoIcons.IoMdClose /> : <FaIcons.FaBars />}
+      </div>
+
+      <div className={showMobileMenu ? "toolbar__tools active" : "toolbar__tools"}>
+        <div className="toolbar__color">
         <label htmlFor="color">Color</label>
         <input
           type="color"
@@ -42,84 +51,85 @@ function Toolbar({
           onChange={handleColor}
           className="color-option"
         />
-      </div>
+        </div>
 
-      <div className="toolbar__size">
-        <label htmlFor="range">Stroke Size</label>
-        <input
-          type="range"
-          name="range"
-          defaultValue="5"
-          min="5"
-          max="100"
-          onChange={handleWidth}
-        />
-      </div>
+        <div className="toolbar__size">
+          <label htmlFor="range">Stroke Size</label>
+          <input
+            type="range"
+            name="range"
+            defaultValue="5"
+            min="5"
+            max="100"
+            onChange={handleWidth}
+          />
+        </div>
 
-      <div className="toolbar__paint">
-        <button
-          onClick={handlePaintMode}
-          className={`${isPaint ? "btn-active" : ""}`}
-        >
-          <FaIcons.FaPaintBrush className="toolbar-icons" />
-        </button>
-      </div>
-
-      <div className="toolbar__line">
-        <button
-          onClick={handleLineMode}
-          className={`${isLine ? "btn-active" : ""}`}
-        >
-          <AiIcons.AiOutlineMenu className="toolbar-icons" />
-        </button>
-      </div>
-
-      <div className="toolbar__rectangle">
-        <button
-          onClick={handleRectangleMode}
-          className={`${isRectangle ? "btn-active" : ""}`}
-        >
-          <BiIcons.BiRectangle className="toolbar-icons" />
-        </button>
-      </div>
-
-      <div className="toolbar__circle">
-        <button
-          onClick={handleCircleMode}
-          className={`${isCircle ? "btn-active" : ""}`}
-        >
-          <BiIcons.BiCircle className="toolbar-icons" />
-        </button>
-      </div>
-
-      <div className="toolbar__eraser">
-        <button
-          onClick={handleEraserMode}
-          className={`${isEraser ? "btn-active" : ""}`}
-        >
-          <FaIcons.FaEraser className="toolbar-icons" />
-        </button>
-      </div>
-
-      <div className="toolbar__download">
-        <a
-          download="image.png"
-          onClick={handleDownload}
-          href={dataUrl}
-          className="toolbar__download-container"
-        >
-          <button className="toolbar-icons">
-            <FaIcons.FaRegSave className="toolbar-icons" />
+        <div className="toolbar__paint">
+          <button
+            onClick={handlePaintMode}
+            className={`${isPaint ? "btn-active" : ""}`}
+          >
+            <FaIcons.FaPaintBrush className="toolbar-icons" />
           </button>
-          <p>Save Image</p>
-        </a>
-      </div>
+        </div>
 
-      <div className="toolbar__clear">
-        <button onClick={modalOpen}>
-          <MdIcons.MdClear className="toolbar-icons" />
-        </button>
-        <p>Clear Canvas</p>
+        <div className="toolbar__line">
+          <button
+            onClick={handleLineMode}
+            className={`${isLine ? "btn-active" : ""}`}
+          >
+            <AiIcons.AiOutlineMenu className="toolbar-icons" />
+          </button>
+        </div>
+
+        <div className="toolbar__rectangle">
+          <button
+            onClick={handleRectangleMode}
+            className={`${isRectangle ? "btn-active" : ""}`}
+          >
+            <BiIcons.BiRectangle className="toolbar-icons" />
+          </button>
+        </div>
+
+        <div className="toolbar__circle">
+          <button
+            onClick={handleCircleMode}
+            className={`${isCircle ? "btn-active" : ""}`}
+          >
+            <BiIcons.BiCircle className="toolbar-icons" />
+          </button>
+        </div>
+
+        <div className="toolbar__eraser">
+          <button
+            onClick={handleEraserMode}
+            className={`${isEraser ? "btn-active" : ""}`}
+          >
+            <FaIcons.FaEraser className="toolbar-icons" />
+          </button>
+        </div>
+
+        <div className="toolbar__download">
+          <a
+            download="image.png"
+            onClick={handleDownload}
+            href={dataUrl}
+            className="toolbar__download-container"
+          >
+            <button className="toolbar-icons">
+              <FaIcons.FaRegSave className="toolbar-icons" />
+            </button>
+            <p>Save Image</p>
+          </a>
+        </div>
+
+        <div className="toolbar__clear">
+          <button onClick={modalOpen}>
+            <MdIcons.MdClear className="toolbar-icons" />
+          </button>
+          <p>Clear Canvas</p>
+        </div>
       </div>
     </div>
   );
